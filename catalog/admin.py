f31db7be97fd04d63d6c744c758373613baa03c4
Daniil Litvinov <daniillitvinov.ru@gmail.com>
2023-02-21 16:17:40 +0300

    Add and set admin classes for all models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import Genre, Book, BookInstance, Author
 
-# Register your models here.
+admin.site.register(Genre)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'last_name', 'first_name',
+        'date_birth', 'date_death'
+    )
+
+    fields = [
+        'first_name', 'last_name', ('date_birth', 'date_death')
+    ]
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.StackedInline):
+    model = BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'author', 'display_genre'
+    )
+
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = (
+        'status', 'due_back'
+    )
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
+
